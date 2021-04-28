96f6f9c3a3946cdea279595bb5bb5f1d369dabbc
dante <ducmanh.tran2904@gmail.com>
2021-04-28 14:27:55 +0700

    feat: Add function and constants

---
+++ src/constants/text.js
@@ -0,0 +1,10 @@
+export const TEXT_LOG_IN = {
+	USERNAME: "Username",
+	PASSWORD: "Password",
+	ERROR: "Invalid username or password.",
+};
+
+export const TEXT_COMMON = {
+	CONFIRM: "Confirm",
+	CANCEL: "Cancel",
+};
--- src/utilities/helper.js
+++ src/utilities/helper.js
@@ -12,3 +12,15 @@ export const downloadFile = (href, fileName) => {
 	aTag.click();
 	document.body.removeChild(aTag);
 };
+
+/**
+ * Replace variables (which is placed between ${}) in a string
+ * @param {string} source input string. E.g. "Hello ${name}!"
+ * @param {object} replaceObj object contains value to replace. E.g. { name: "Katorin" }
+ * Result from above inputs is "Hello Katorin!"
+ */
+export const insertString = (source, replaceObj) =>
+	Object.entries(replaceObj).reduce((acc, [key, value]) => {
+		const regEx = new RegExp(`\\\${\\s*${key}\\s*}`, "g");
+		return acc.replace(regEx, value);
+	}, source);
