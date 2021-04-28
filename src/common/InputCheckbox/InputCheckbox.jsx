45120d0defd3908b4f22170691816a4c30e074f4
dante <ducmanh.tran2904@gmail.com>
2021-04-28 11:16:44 +0700

    feat: Button and InputCheckbox

---
+++ src/common/InputCheckbox/InputCheckbox.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Checkbox } from "@chakra-ui/checkbox";
+
+const InputCheckbox = ({
+	className,
+	label,
+	name,
+	value,
+	colorScheme = "teal", // teal | blue | cyan | green | orange | pink | purple | red | yellow
+	size = "md", // xs | sm | md | lg
+	isRequired,
+	isDisabled,
+	onChange,
+	...props
+}) => (
+	<Checkbox
+		{...props}
+		className={className}
+		name={name}
+		value={value}
+		size={size}
+		colorScheme={colorScheme}
+		onChange={onChange}
+		isRequired={isRequired}
+		isDisabled={isDisabled}
+		isChecked={!!value}
+	>
+		{label}
+	</Checkbox>
+);
+
+export default InputCheckbox;
