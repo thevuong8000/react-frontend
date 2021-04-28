71f684cdd1b4c9cc92784cd99246b977ba49c9c3
dante <ducmanh.tran2904@gmail.com>
2021-04-28 20:20:56 +0700

    fix: Config env

---
+++ src/constants/configs.js
@@ -0,0 +1,14 @@
+export const APP_CONFIG = {
+	API_ROOT: process.env.REACT_APP_API_DOMAIN
+};
+
+export const API_PATH = {};
+
+export const ROUTE = {
+	HOME: {
+		to: '/'
+	},
+	LOGIN: {
+		to: '/login'
+	}
+};
