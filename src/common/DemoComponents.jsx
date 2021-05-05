759815f5d414771400ce81fe780fdbd58787ee71
dante <dante@cinnamon.is>
2021-05-05 17:23:01 +0700

    feat: Create Demo-Component page

---
+++ src/common/DemoComponents.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const DemoComponents = () => <div>Demo Components</div>;
+
+export default DemoComponents;
--- src/pages/AuthenticatedPage/AuthenticatedPage.jsx
+++ src/pages/AuthenticatedPage/AuthenticatedPage.jsx
@@ -1,11 +1,11 @@
 import { Box, Flex } from '@chakra-ui/layout';
 import Header from '@common/Header/Header';
 import { ROUTE } from '@constants/routing';
-import { PAGE_TITLE } from '@constants/text';
 import React, { lazy } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 const Homepage = lazy(() => import('../Homepage/Homepage'));
+const DemoComponents = lazy(() => import('@common/DemoComponents'));
 
 const AuthenticatedPage = () => (
   <Router>
@@ -16,12 +16,18 @@ const AuthenticatedPage = () => (
           <Route
             exact
             path={ROUTE.HOME.to}
-            render={() => <Homepage documentTitle={PAGE_TITLE.HOMEPAGE} />}
+            render={() => <Homepage documentTitle={ROUTE.HOME.title} />}
+          />
+
+          <Route
+            exact
+            path={ROUTE.DEMO.to}
+            render={() => <DemoComponents documentTitle={ROUTE.DEMO.title} />}
           />
 
           {/* Default fall back to Homepage */}
           <Route path="*">
-            <Redirect to={ROUTE.HOME.to} />
+            <Redirect to={ROUTE.DEMO.to} />
           </Route>
         </Switch>
       </Box>
