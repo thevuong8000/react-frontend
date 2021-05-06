64b9e76e7c26d42f9bbacdad321baad19d452304
dante <ducmanh.tran2904@gmail.com>
2021-05-06 21:09:00 +0700

    refactor(demo-constants): Generalize styles to variables

---
+++ src/demo-components/demo-helper/constants.js
@@ -0,0 +1,18 @@
+export const STYLE = {
+	DEMO_WRAPPER: {
+		direction: 'row',
+		width: '100%',
+		justifyContent: 'space-between',
+		align: 'center'
+	},
+
+	COMPONENT_OPTIONS: {
+		direction: 'column'
+	},
+
+	COMPONENT_RENDER: {
+		width: '50%',
+		justifyContent: 'center',
+		align: 'center'
+	}
+};
