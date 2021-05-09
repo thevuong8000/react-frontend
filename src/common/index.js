0e57c7ca6ee98a1c59595651e5f527e9f23dfb71
dante <ducmanh.tran2904@gmail.com>
2021-05-09 22:50:16 +0700

    feat: Add index file for @common

---
+++ src/common/index.js
@@ -0,0 +1,7 @@
+export { default as Header } from './Header/Header';
+export { default as Footer } from './Footer/Footer';
+export { default as InputRadio } from './InputRadio/InputRadio';
+export { default as InputText } from './InputText/InputText';
+export { default as InputCheckbox } from './InputCheckbox';
+export { default as Selector } from './Selector/Selector';
+export { default as Table } from './Table/Table';
