45fed622631516b5cfd1dad077137db83c8afa71
dante_cin <56090625+thevuong8000@users.noreply.github.com>
2021-05-28 16:59:36 +0700

    fix: Render Full Loader in the first page load (#15)

--- src/contexts/auth-provider.jsx
+++ src/contexts/auth-provider.jsx
@@ -1,3 +1,5 @@
+import { useBoolean } from '@chakra-ui/hooks';
+import LoaderFull from '@common/LoaderFull/LoaderFull';
 import { API_PATH } from '@constants/configs';
 import useApi from '@hooks/useApi';
 import { clearLoginInfo, getLoginInfo, saveLoginInfo } from '@utilities/helper';
@@ -9,12 +11,16 @@ const AuthProvider = ({ children }) => {
   const [data, setData] = useState(null);
   const { apiPost } = useApi();
 
+  /* Test-token for the first page load */
+  const [finishFirstLoad, setFinishFirstLoad] = useBoolean(false);
+
   /* Verify the access_token with user id */
   useEffect(() => {
     const storedInfo = getLoginInfo();
     const { id, access_token } = storedInfo;
     apiPost(API_PATH.AUTH.TEST_TOKEN, { id, access_token }).then(() => setData(storedInfo));
 
+    setFinishFirstLoad.on();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -35,6 +41,8 @@ const AuthProvider = ({ children }) => {
     clearLoginInfo();
     setData(null);
   }, []);
+
+  if (!finishFirstLoad) return <LoaderFull />;
 
   return (
     <AuthContext.Provider value={{ user: data, logIn, logOut }}>{children}</AuthContext.Provider>
