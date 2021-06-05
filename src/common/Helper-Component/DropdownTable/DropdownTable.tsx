4f1ba899d091cd8877b797c17f9926c899d9f9b4
dante <ducmanh.tran2904@gmail.com>
2021-06-05 23:19:28 +0700

    refactor: Dropdown Table

--- src/common/Helper-Component/DropdownTable/DropdownTable.tsx
+++ src/common/Helper-Component/DropdownTable/DropdownTable.tsx
@@ -1,42 +1,55 @@
-import React, { forwardRef } from 'react';
+import React, { FC, forwardRef, MouseEventHandler, ReactElement } from 'react';
 import { Button } from '@chakra-ui/button';
 import { Box, Text, VStack } from '@chakra-ui/layout';
 import { useColorModeValue, useTheme } from '@chakra-ui/system';
 import useToggle from '@hooks/useToggle';
 import { TiTick } from 'react-icons/ti';
-import Icon from '@chakra-ui/icon';
+import ChakraIcon from '@chakra-ui/icon';
 import { Tooltip } from '@chakra-ui/tooltip';
-import { bool, string, element, func, arrayOf, shape } from 'prop-types';
 
-const DropdownTable = forwardRef((props, ref) => {
+export interface IDropdownOption {
+  text: string;
+  isDisabled?: boolean;
+  Icon?: ReactElement;
+}
+
+interface IDropdown {
+  name: string;
+  selectedOptions: string[];
+  options: IDropdownOption[];
+  isOpen: boolean;
+  onSelect: MouseEventHandler;
+}
+
+const DropdownTable = forwardRef<HTMLDivElement, IDropdown>((props, ref) => {
   const { name, selectedOptions = [], options = [], isOpen, onSelect } = props;
 
   const { zIndices } = useTheme();
   const opacity = useToggle(isOpen, { trueValue: '1', falseValue: '0' });
   const visibility = useToggle(isOpen, { trueValue: 'visible', falseValue: 'hidden' });
 
-  const DropdownItems = ({ option }) => {
-    const isChecked = selectedOptions.includes(option.text);
+  const DropdownItems: FC<IDropdownOption> = ({ text, isDisabled, Icon }) => {
+    const isChecked = selectedOptions.includes(text);
     const checkedVisibility = useToggle(isChecked, { trueValue: 'visible', falseValue: 'hidden' });
     return (
-      <Tooltip label={option.text} hasArrow placement="auto">
+      <Tooltip label={text} hasArrow placement="auto">
         <Button
           w="100%"
           variant="ghost"
           colorScheme="gray"
           size="sm"
-          value={option.text}
+          value={text}
           onClick={onSelect}
           pt="2"
           pb="2"
           justifyContent="space-between"
-          isDisabled={option.isDisabled}
-          leftIcon={option.icon}
+          isDisabled={isDisabled}
+          leftIcon={Icon}
         >
           <Text maxW="85%" textOverflow="ellipsis" overflow="hidden" pointerEvents="none">
-            {option.text}
+            {text}
           </Text>
-          <Icon as={TiTick} color="green" visibility={checkedVisibility} />
+          <ChakraIcon as={TiTick} color="green" visibility={checkedVisibility} />
         </Button>
       </Tooltip>
     );
@@ -61,25 +74,16 @@ const DropdownTable = forwardRef((props, ref) => {
         transition="all 0.1s ease"
       >
         {options.map((option, index) => (
-          <DropdownItems key={`${name}-option-${index}`} option={option} />
+          <DropdownItems
+            key={`${name}-option-${index}`}
+            text={option.text}
+            isDisabled={option.isDisabled}
+            Icon={option.Icon}
+          />
         ))}
       </VStack>
     </Box>
   );
 });
-
-DropdownTable.propTypes = {
-  name: string,
-  selectedOptions: arrayOf(string).isRequired,
-  options: arrayOf(
-    shape({
-      text: string,
-      isDisabled: bool,
-      Icon: element
-    })
-  ),
-  isOpen: bool.isRequired,
-  onSelect: func.isRequired
-};
 
 export default DropdownTable;
