7c4f16c82e94fc0b987f5c7593da95bf10d97e73
dante <ducmanh.tran2904@gmail.com>
2021-06-05 22:40:03 +0700

    refactor: Chevron

--- src/common/AnimatedIcons/Chevron.tsx
+++ src/common/AnimatedIcons/Chevron.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
+import React, { FC } from 'react';
 import Icon from '@chakra-ui/icon';
 import { BsChevronDown } from 'react-icons/bs';
-import { bool } from 'prop-types';
 
-const AnimatedChevron = ({ isRotated }) => (
+interface IChevron {
+  isRotated: boolean;
+}
+
+const AnimatedChevron: FC<IChevron> = ({ isRotated }) => (
   <Icon
     as={BsChevronDown}
     transform={`rotate(${isRotated ? 0 : 180}deg)`}
     transition="all 0.3s ease"
   />
 );
-
-AnimatedChevron.propTypes = {
-  isRotated: bool
-};
 
 export default AnimatedChevron;
