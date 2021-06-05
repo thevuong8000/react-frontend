4b68b0d4f9d0f8ded96bfb03444dcdc8d0d6659a
dante <ducmanh.tran2904@gmail.com>
2021-06-05 20:32:36 +0700

    fix: Webpack

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,16 +1,17 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
+import { merge } from 'webpack-merge';
+import common from './webpack.common.js';
 
-module.exports = merge(common, {
-  mode: "development",
-  devtool: "cheap-source-map",
-  devServer: {
-    host: "0.0.0.0",
-    port: 3000,
-    hot: true,
-    disableHostCheck: true,
-    historyApiFallback: true,
-    progress: true,
-    publicPath: "/",
-  },
+export default merge(common, {
+	mode: 'development',
+	devtool: 'cheap-source-map',
+	devServer: {
+		host: '0.0.0.0',
+		port: 3000,
+		hot: true,
+		disableHostCheck: true,
+		historyApiFallback: true,
+		progress: true,
+		publicPath: '/',
+		open: true
+	}
 });
