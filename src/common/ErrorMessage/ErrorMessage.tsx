3bdb9aca000041c1aace55d20aaec64c8cb494bc
dante <ducmanh.tran2904@gmail.com>
2021-06-05 22:41:26 +0700

    refactor: Error Message

--- src/common/ErrorMessage/ErrorMessage.tsx
+++ src/common/ErrorMessage/ErrorMessage.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { chakra, useColorModeValue } from '@chakra-ui/system';
 import { Text } from '@chakra-ui/layout';
 import { IoWarningSharp } from 'react-icons/io5';
 
-const ErrorMessage = ({ message, ...props }) => {
+interface Message {
+  message: string;
+}
+
+const ErrorMessage: FC<Message> = ({ message, ...props }) => {
   const color = useColorModeValue('red.500', 'red.300');
   return (
     <Text colorScheme="red" color={color} d="inline-flex" alignItems="center" {...props}>
