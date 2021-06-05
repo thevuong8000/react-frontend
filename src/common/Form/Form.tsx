1d62c5ed18e55299f2025e9c23b80bd160b946d9
dante <ducmanh.tran2904@gmail.com>
2021-06-05 22:47:09 +0700

    refactor: Form

--- src/common/Form/Form.tsx
+++ src/common/Form/Form.tsx
@@ -1,10 +1,18 @@
 import { Button } from '@chakra-ui/button';
-import React from 'react';
+import React, { CSSProperties, FC, FormEvent } from 'react';
+
+interface IForm {
+  style: CSSProperties;
+  onSubmit(e: FormEvent): void;
+
+  // others
+  [key: string]: any;
+}
 
 const DEFAULT_STYLE = {
   width: '100%'
 };
-const Form = ({ children, style, onSubmit, ...props }) => (
+const Form: FC<IForm> = ({ children, style, onSubmit, ...props }) => (
   <form onSubmit={onSubmit} style={{ ...DEFAULT_STYLE, ...style }} {...props}>
     {children}
     <Button type="submit" d="none" />
