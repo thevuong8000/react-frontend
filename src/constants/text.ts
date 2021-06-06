5a86e3efabf2fa5f99afba45735dc4206b9e1cc8
dante <ducmanh.tran2904@gmail.com>
2021-06-06 18:16:47 +0700

    refactor: Text

---
+++ src/constants/text.ts
@@ -0,0 +1,34 @@
+export const TEXT_LOG_IN = {
+  USERNAME: 'username',
+  PASSWORD: 'password',
+  ERROR_LOGIN: 'Invalid username or password.',
+  LOGIN: 'Login',
+  CREATE_ACCOUNT: 'Create Account'
+};
+
+export const TEXT_COMMON = {
+  APP_TITLE: 'REACT-FRONTEND' /* This is the title for the app */,
+
+  CONFIRM: 'Confirm',
+  CANCEL: 'Cancel',
+
+  SAVE: 'Save'
+};
+
+export const TEXT_HEADER = {
+  PROFILE: {
+    SETTINGS: 'Settings',
+    LOG_OUT: 'Log out',
+    FEEDBACK: 'Give Feedback',
+    CHANGE_PASSWORD: 'Change Password'
+  }
+};
+
+export const TEXT_PROFILE = {
+  CURRENT_PASSWORD: 'Current Password',
+  NEW_PASSWORD: 'New Password',
+  CONFIRM_PASSWORD: 'Confirm Password',
+  PASSWORD_PLACEHOLDER: 'password...',
+  CHANGE_PASSWORD_SUCCESS: 'Change password successfully',
+  CHANGE_PASSWORD_FAILURE: 'Failed to change password'
+};
