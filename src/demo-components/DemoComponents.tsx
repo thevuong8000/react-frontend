0baeeecdc99626abd55c97a7df8f0ed389de6f2b
dante <ducmanh.tran2904@gmail.com>
2021-07-20 00:06:22 +0700

    feat: Upload Files

--- src/demo-components/DemoComponents.tsx
+++ src/demo-components/DemoComponents.tsx
@@ -4,6 +4,7 @@ import TableDemo from './Table/TableDemo';
 import InputRadioDemo from './InputRadio/InputRadioDemo';
 import SelectorDemo from './Selector/SelectorDemo';
 import { PageBase } from 'paging';
+import FileUploadDemo from './FileUpload/FileUploadDemo';
 
 interface IDemo {
   title: string;
@@ -25,13 +26,15 @@ const COMPONENT_NAME = {
   INPUT_CHECKBOX: 'Input Checkbox',
   INPUT_RADIO: 'Input Radio',
   TABLE: 'Table',
-  SELECTOR: 'Selector'
+  SELECTOR: 'Selector',
+  FILE_UPLOAD: 'File Upload'
 };
 
 const items: Array<IDemoItem> = [
   { title: COMPONENT_NAME.SELECTOR, component: <SelectorDemo /> },
   { title: COMPONENT_NAME.INPUT_RADIO, component: <InputRadioDemo /> },
-  { title: COMPONENT_NAME.TABLE, component: <TableDemo /> }
+  { title: COMPONENT_NAME.TABLE, component: <TableDemo /> },
+  { title: COMPONENT_NAME.FILE_UPLOAD, component: <FileUploadDemo /> }
 ];
 
 const DemoComponents: FC<PageBase> = ({ documentTitle }) => {
