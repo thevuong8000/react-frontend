0e99d4b69945d3af2ef2d783bcdde61987a2ae7a
dante <ducmanh.tran2904@gmail.com>
2021-08-06 09:56:18 +0700

    fix: Candra title

--- src/constants/text.ts
+++ src/constants/text.ts
@@ -1,34 +1,34 @@
 export const TEXT_LOG_IN = {
-  USERNAME: 'username',
-  PASSWORD: 'password',
-  ERROR_LOGIN: 'Invalid username or password.',
-  LOGIN: 'Login',
-  CREATE_ACCOUNT: 'Create Account'
+	USERNAME: 'username',
+	PASSWORD: 'password',
+	ERROR_LOGIN: 'Invalid username or password.',
+	LOGIN: 'Login',
+	CREATE_ACCOUNT: 'Create Account'
 };
 
 export const TEXT_COMMON = {
-  APP_TITLE: 'REACT-FRONTEND' /* This is the title for the app */,
+	APP_TITLE: process.env.APP_TITLE,
 
-  CONFIRM: 'Confirm',
-  CANCEL: 'Cancel',
+	CONFIRM: 'Confirm',
+	CANCEL: 'Cancel',
 
-  SAVE: 'Save'
+	SAVE: 'Save'
 };
 
 export const TEXT_HEADER = {
-  PROFILE: {
-    SETTINGS: 'Settings',
-    LOG_OUT: 'Log out',
-    FEEDBACK: 'Give Feedback',
-    CHANGE_PASSWORD: 'Change Password'
-  }
+	PROFILE: {
+		SETTINGS: 'Settings',
+		LOG_OUT: 'Log out',
+		FEEDBACK: 'Give Feedback',
+		CHANGE_PASSWORD: 'Change Password'
+	}
 };
 
 export const TEXT_PROFILE = {
-  CURRENT_PASSWORD: 'Current Password',
-  NEW_PASSWORD: 'New Password',
-  CONFIRM_PASSWORD: 'Confirm Password',
-  PASSWORD_PLACEHOLDER: 'password...',
-  CHANGE_PASSWORD_SUCCESS: 'Change password successfully',
-  CHANGE_PASSWORD_FAILURE: 'Failed to change password'
+	CURRENT_PASSWORD: 'Current Password',
+	NEW_PASSWORD: 'New Password',
+	CONFIRM_PASSWORD: 'Confirm Password',
+	PASSWORD_PLACEHOLDER: 'password...',
+	CHANGE_PASSWORD_SUCCESS: 'Change password successfully',
+	CHANGE_PASSWORD_FAILURE: 'Failed to change password'
 };
