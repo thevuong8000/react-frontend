8990b839eceef04f590deaa07e49234206d02054
katorin <56090625+thevuong8000@users.noreply.github.com>
2021-08-10 14:16:29 +0700

    Candra standalone functions style (#51)
    * feat: Icons for functional buttons
    
    * fix: Icon size

--- src/pages/Candra/CandraFunctions.tsx
+++ src/pages/Candra/CandraFunctions.tsx
@@ -2,6 +2,7 @@ import { Button, Flex, Select } from '@chakra-ui/react';
 import { Language } from '@common/CodeEditor/CodeEditor';
 import { SUPPORTED_LANGUAGES } from '@constants/code-executor';
 import React, { ChangeEventHandler, FC } from 'react';
+import { VscCollapseAll, VscExpandAll, VscAdd, VscRunAll } from 'react-icons/vsc';
 
 interface ICandraHeaderFunctions {
   initLanguage: Language;
@@ -34,16 +35,31 @@ const CandraFunctions: FC<ICandraHeaderFunctions> = ({
           </option>
         ))}
       </Select>
-      <Button size="md" onClick={handleAddTest}>
+      <Button size="md" onClick={handleAddTest} colorScheme="cyan" rightIcon={<VscAdd size={18} />}>
         Add Test
       </Button>
-      <Button size="md" onClick={handleCollapseAll}>
+      <Button
+        size="md"
+        onClick={handleCollapseAll}
+        colorScheme="blue"
+        rightIcon={<VscCollapseAll size={18} />}
+      >
         Collapse All
       </Button>
-      <Button size="md" onClick={handleExpendAll}>
+      <Button
+        size="md"
+        onClick={handleExpendAll}
+        colorScheme="teal"
+        rightIcon={<VscExpandAll size={18} />}
+      >
         Expand All
       </Button>
-      <Button size="md" onClick={handleExecuteCode}>
+      <Button
+        size="md"
+        onClick={handleExecuteCode}
+        colorScheme="green"
+        rightIcon={<VscRunAll size={18} />}
+      >
         Execute
       </Button>
     </Flex>
