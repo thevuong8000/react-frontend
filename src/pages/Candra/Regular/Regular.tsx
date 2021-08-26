3e100fb60a19ee1fd570bf3332de9dfbda6d4d0b
thevuong8000 <ducmanh.tran2904@gmail.com>
2021-08-26 09:50:01 -0400

    feat: Create Regular tab

---
+++ src/pages/Candra/Regular/Regular.tsx
@@ -0,0 +1,40 @@
+import { Flex, Text, ThemeTypings } from '@chakra-ui/react';
+import React, { FC } from 'react';
+
+interface IRegularSuccess {
+  status: 'Success';
+  detail: string;
+}
+
+interface IRegularFailure {
+  status: 'Compile Error' | 'Runtime Error';
+  detail: string;
+}
+
+type IRegular = IRegularSuccess | IRegularFailure;
+
+const getColorSchemeByStatus = (status: IRegular['status']): ThemeTypings['colorSchemes'] => {
+  switch (status) {
+    case 'Success':
+      return 'green';
+
+    case 'Compile Error':
+    case 'Runtime Error':
+      return 'red';
+
+    default:
+      return 'gray';
+  }
+};
+
+const Regular: FC<IRegular> = ({ status, detail }) => {
+  const colorScheme = getColorSchemeByStatus(status);
+  return (
+    <Flex>
+      <Text colorScheme={colorScheme}>{status}</Text>
+      <Text>{detail}</Text>
+    </Flex>
+  );
+};
+
+export default Regular;
